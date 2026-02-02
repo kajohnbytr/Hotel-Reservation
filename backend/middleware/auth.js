4d56d5f9f192f6a68bb1ef9f92540e8641c35e06
PaulSrzb <sarzabapaulvincent@gmail.com>
2026-02-03 00:50:05 +0800

    fix db connection

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,4 +1,4 @@
-import User from "../models/user";
+import User from "../models/user.js";
 import jwt from "jsonwebtoken";
 
 export const protect = async (req, res, next) => {
@@ -7,16 +7,16 @@ export const protect = async (req, res, next) => {
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
             token = req.headers.authorization.split(' ')[1];
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        req.user = await User.findById(decoded.id).select('-password');
-
-        return next();
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.id).select('-password');
+            return next();
         } catch (error) {
-            console.error("");
-            res.status(401).json({ message: 'Not authorized, token failed' });
+            console.error(error);
+            return res.status(401).json({ message: 'Not authorized, token failed' });
         }
     }
-    res.status(401).json({ message: 'Not authorized, token failed' });
+    
+    if (!token) {
+        return res.status(401).json({ message: 'Not authorized, no token' });
+    }
 }
